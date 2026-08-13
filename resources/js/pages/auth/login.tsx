09d3412307454e8d9f5367531f20dfcc07d15bb2
xFardi <47803063+blad99@users.noreply.github.com>
2026-08-13 20:27:01 +0700

    feat: change login identifier from email to username

--- resources/js/pages/auth/login.tsx
+++ resources/js/pages/auth/login.tsx
@@ -29,18 +29,17 @@ export default function Login({ status, canResetPassword }: Props) {
                     <>
                         <div className="grid gap-6">
                             <div className="grid gap-2">
-                                <Label htmlFor="email">Email address</Label>
+                                <Label htmlFor="username">Username</Label>
                                 <Input
-                                    id="email"
-                                    type="email"
-                                    name="email"
+                                    id="username"
+                                    type="text"
+                                    name="username"
                                     required
                                     autoFocus
                                     tabIndex={1}
-                                    autoComplete="email"
-                                    placeholder="email@example.com"
+                                    placeholder="Username"
                                 />
-                                <InputError message={errors.email} />
+                                <InputError message={errors.username} />
                             </div>
 
                             <div className="grid gap-2">
